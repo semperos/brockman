bb5adfe227bf8b89aa7fb6f75c3aca2d346f9376
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2015-10-28 11:25:08 -0400

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.semperos/brockman "0.1.0"
+(defproject com.semperos/brockman "0.2.0-SNAPSHOT"
   :description "Custom test reporting for clojure.test"
   :url "https://github.com/semperos/brockman"
   :license {:name "Eclipse Public License"
