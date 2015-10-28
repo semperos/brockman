6769fcc24e9964a82360163e40975b8adbe66f8e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2015-10-28 10:24:24 -0400

    Add Clojars as release repo

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [colorstr "0.1.0"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.8.2"]
                                   [com.gfredericks/test.chuck "0.2.0"]]}})
